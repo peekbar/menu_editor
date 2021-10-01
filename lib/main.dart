ebb3c709ad9b092f9205bf9e9b090df87ff65852
TheOlaf98 <d.schaubschlaeger@icloud.com>
2021-10-01 12:14:14 +0200

    added configurable window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:math' as math;
 import 'package:menu_editor/controllers/menus_controller.dart';
 import 'package:menu_editor/menu_editor.dart';
+
+import 'package:window_size/window_size.dart' as window_size;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,5 +13,21 @@ Future<void> main() async {
 }
 
 Future<void> preAppInit() async {
+  setWindowSize();
   Get.put(MenusController(), permanent: true);
+}
+
+setWindowSize() {
+  window_size.getWindowInfo().then((window) {
+    final screen = window.screen;
+    if (screen != null) {
+      final screenFrame = screen.visibleFrame;
+      final width = math.min(screenFrame.width, 1000.0);
+      final height = math.min(screenFrame.height, 1000.0);
+      final left = ((screenFrame.width - width) / 2).roundToDouble();
+      final top = ((screenFrame.height - height) / 3).roundToDouble();
+      final frame = Rect.fromLTWH(left, top, width, height);
+      window_size.setWindowFrame(frame);
+    }
+  });
 }
